8b1fa6445965086d04a379bd3bb40067c6f609bd
Ankit07kumar11 <ankitkumar56074@gmail.com>
2023-03-22 00:37:05 +0530

    heroimage updated

--- src/components/utils/Sociallinks.jsx
+++ src/components/utils/Sociallinks.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SocialLink = ({icon}) => {
   return (
     <div>
-      <img src={icon} alt="icon/social"  className='w-8 h-8 cursor-pointer md:h-6 md:w-6 sm:w-5 sm:h-5 transition-all duration-200 hover:scale-110'/>
+      <img src={icon} alt="icon/social"  className='w-8 h-8 flex items-center cursor-pointer md:h-6 md:w-6 sm:w-5 sm:h-5 transition-all duration-200 hover:scale-110'/>
       
     </div>
   )
